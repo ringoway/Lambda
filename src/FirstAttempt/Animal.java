cefdff37d1dfe42793ad8d6bb7b9649052dd5e0e
Azat <azat.takhautdinov@gmail.com>
2022-10-22 22:41:47 +0500

    update code. Added sorting by maxSpeed from smallest to largest.

--- src/FirstAttempt/Animal.java
+++ src/FirstAttempt/Animal.java
@@ -1,10 +1,13 @@
 package FirstAttempt;
 
-public class Animal {
-    private String species;
+import static java.lang.Integer.parseInt;
+
+public class Animal implements Comparable{
+    private final String species;
     private boolean canFastRun;
-    private boolean canSwim;
-    int maxSpeed;
+    private final boolean canSwim;
+    private final int maxSpeed;
+    String pars;
 
     public Animal(String species, int maxSpeed, boolean canSwim){
         this.species = species;
@@ -19,6 +22,18 @@ public class Animal {
         return maxSpeed > 40;
     }
     public String toString() {
+        return species+" and his speed: "+maxSpeed+" km/h";
+    }
+
+    public String getSpecies() {
         return species;
+    }
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public int compareTo(Object o) {
+        Animal a = (Animal) o;
+        return this.maxSpeed - a.maxSpeed;
     }
 }
