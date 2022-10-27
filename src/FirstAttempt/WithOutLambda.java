afc6d6dc455067b578bc711216638ff85c2f6b48
Azat <azat.takhautdinov@gmail.com>
2022-10-27 23:06:44 +0500

    test

--- src/FirstAttempt/WithOutLambda.java
+++ src/FirstAttempt/WithOutLambda.java
@@ -1,29 +1,29 @@
-package FirstAttempt;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class WithOutLambda {
-    public static void main(String[] args) {
-        List<Animal> animals = new ArrayList<>();
-
-        animals.add(new Animal("dog", 20, true));
-        animals.add(new Animal("leopard", 58, true));
-        animals.add(new Animal("panther", 88, true));
-        animals.add(new Animal("horse", 89, true));
-        animals.add(new Animal("turtle", 5, true));
-        animals.add(new Animal("monkey", 15, false));
-        animals.add(new Animal("cheetah", 130, true));
-
-        print(animals, new CheckIfFastRun());
-    }
-    private static void print(List<Animal> animals1, CheckTrait checker){
-        for (Animal animal : animals1) {
-            if (checker.test(animal))
-                System.out.println(animal + " ");
-        }
-        System.out.println(" ");
-    }
-
-}
-
+//package FirstAttempt;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class WithOutLambda {
+//    public static void main(String[] args) {
+//        List<Animal> animals = new ArrayList<>();
+//
+//        animals.add(new Animal("dog", 20, true));
+//        animals.add(new Animal("leopard", 58, true));
+//        animals.add(new Animal("panther", 88, true));
+//        animals.add(new Animal("horse", 89, true));
+//        animals.add(new Animal("turtle", 5, true));
+//        animals.add(new Animal("monkey", 15, false));
+//        animals.add(new Animal("cheetah", 130, true));
+//
+//        print(animals, new CheckIfFastRun());
+//    }
+//    private static void print(List<Animal> animals1, CheckTrait checker){
+//        for (Animal animal : animals1) {
+//            if (checker.test(animal))
+//                System.out.println(animal + " ");
+//        }
+//        System.out.println(" ");
+//    }
+//
+//}
+//
---
+++ src/SecondAttempt/Car.java
@@ -0,0 +1,4 @@
+package SecondAttempt;
+
+public class Car {
+}
