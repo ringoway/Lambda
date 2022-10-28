fd0d36321b191b345e1b62dcb33de46159c3973c
Azat <azat.takhautdinov@gmail.com>
2022-10-28 23:10:16 +0500

    update

---
+++ src/SecondAttempt/CheckTrait.java
@@ -0,0 +1,5 @@
+package SecondAttempt;
+
+public interface CheckTrait {
+    boolean test(Car x);
+}
--- src/SecondAttempt/Main.java
+++ src/SecondAttempt/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) {
         List<Car> cars = new ArrayList<>();
 
-
+        cars.add(new Car("Ford", "Focus", 2021, 220, 150) );
     }
 }
