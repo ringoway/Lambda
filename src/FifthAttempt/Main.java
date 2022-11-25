46eb4fc724a7203d7a7a8069273bff08ad3d4e87
Azat <azat.takhautdinov@gmail.com>
2022-11-25 23:14:13 +0500

    update

--- src/FifthAttempt/Main.java
+++ src/FifthAttempt/Main.java
@@ -1,7 +1,16 @@
 package FifthAttempt;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person("Egor", 168, 65));
+        personList.add(new Person("Gleb", 190, 80));
 
+        Collections.sort(personList);
+        personList.forEach(Person::showGrowth);
     }
 }
--- src/FifthAttempt/Person.java
+++ src/FifthAttempt/Person.java
@@ -1,13 +1,24 @@
 package FifthAttempt;
 
-public class Person {
+public class Person implements Comparable<Person>{
     private String name;
-    private double growth;
+    private int growth;
     private double weight;
 
-    public Person(String name, double growth, double weight) {
+    public Person(String name, int growth, double weight) {
         this.name = name;
         this.growth = growth;
         this.weight = weight;
+    }
+    public void showGrowth(){
+        System.out.println(name+growth);
+    }
+
+    @Override
+    public int compareTo(Person person) {
+        return this.growth = person.growth;
+    }
+    public String toString(){
+        return name + " her growth: "+growth;
     }
 }
